fd31ec5cb67191e7656ae8780c31388afd67eb48
JustinLap <justinlaprise02@gmail.com>
2025-09-26 15:46:12 -0400

    FIN

--- Assets/Scripts/Character/JammoPlayer.cs
+++ Assets/Scripts/Character/JammoPlayer.cs
@@ -12,8 +12,53 @@ public class JammoPlayer : MonoBehaviour
 
     // TODO : Compléter cette classe.
 
+    private CharacterController characterController;
+
+
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Obtenir des vecteurs de direction relatifs a la caméra
+
+        var camera = Camera.main!;
+        var cameraTransform = camera.transform;
+        var forward = cameraTransform.forward;
+        var right = cameraTransform.right;
+
+        // Retirer la rotation x et z (garder le mouvement horizontal)
+
+        forward.y = 0;
+        right.y = 0;
+
+        // Lire les entrées du joueur.
+        var moveInput = moveAction.action.ReadValue<Vector2>();
+
+
+        // Si le jroueur veut pas bouger, ne pas faire bouger le joueur.
+
+        if (moveInput == Vector2.zero)
+        {
+            characterController.Move(Vector3.zero);
+        }
+        else
+        {
+            // Y multiplie forward (avance/recule).
+            // X multiplie right (gauche/droite).
+            //Combinaison des deux fait le mouvement réel.
+
+            var moveDirection = forward * moveInput.y + right * moveInput.x;
+
+            characterController.Move(moveDirection * (speed * Time.deltaTime));
+
+
+            var lookRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed);
+
+        }
     }
 }
